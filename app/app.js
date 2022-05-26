597770a59a5f712a445f90bd87de20d88367b128
ALVINdimpos <alvindimpos@gmail.com>
2022-05-26 13:16:09 +0200

    car-sho

--- app/app.js
+++ app/app.js
@@ -1,64 +1,73 @@
-const carListOb = {
-  carlist: [
-    {
-      title: "Cheverloret Camaro",
-      imgUrl:
-        "https://www.gmignitionupdate.com/dld/content/dam/Media/images/INTL/chevrolet/passenger-cars/camaro-coupe/2013/static-shots/Chevrolet-Camaro-Coupe-266806.jpg",
-      price: 230000,
-      description: "A better and fast car",
-    },
-    {
-      title: "Hyundai Tucson",
-      imgUrl:
-        "https://di-enrollment-api.s3.amazonaws.com/hyundai/models/2022/tucson/colors/shimmering_silver.jpg",
-      price: 260000,
-      description: "A better and family car",
-    },
-    {
-      title: "Hyundai Tucson",
-      imgUrl:
-        "https://www.indiacarnews.com/wp-content/uploads/2022/03/2022-Hyundai-Tucson-7-Seater.jpg",
-      price: 260000,
-      description: "A better and family car",
-    },
-    {
-      title: "Cheverloret Camaro",
-      imgUrl:
-        "https://www.chevrolet.ca/content/dam/chevrolet/na/canada/english/index/performance/2022-camaro/mov/new/2022-camaro-masthead-01.jpg?imwidth=960",
-      price: 230000,
-      description: "A better and fast car",
-    },
-  ],
-  render() {
-    const rootApp = document.getElementById("app");
+// import axios from 'axios'
+import { Car } from "./models/car.js";
+import { CarElement } from "./carElement.js";
+
+class CarList {
+  carlist = [
+    new Car(
+      "Cheverloret Camaro",
+      "https://www.chevrolet.ca/content/dam/chevrolet/na/canada/english/index/performance/2022-camaro/mov/new/2022-camaro-masthead-01.jpg?imwidth=960",
+      200000,
+      "A better and fast car"
+    ),
+    new Car(
+      "Hyundai Tucson",
+      "https://www.indiacarnews.com/wp-content/uploads/2022/03/2022-Hyundai-Tucson-7-Seater.jpg",
+      203000,
+      "A better and family car"
+    ),
+  ];
+
+  // todo create the class called CarElement and then
+  // todo  paste the car element inside then call it inside the carList
+
+  async render() {
+    const rootApp = document.querySelector("#app");
     const header = document.createElement("div");
+    const main = document.getElementById("main");
     header.className = "header";
     header.innerHTML = `
     <div class="header-title" >
     <div>Car SHop</div>
-    <div>Cart</div>
+    <a href="../car.html"><div class="add-car">Add Car</div></a>
     </div>
     `;
 
     const carListDOM = document.createElement("ul");
     carListDOM.className = "list";
-    for (let car of this.carlist) {
-      const carItem = document.createElement("li");
-      carItem.innerHTML = `
-      <div  >
-      <div class="list-image">
-      <img src="${car.imgUrl}" alt="${car.title}"/>
-      </div>
-      <div class="title">${car.title}</div>
-      <div class="description">${car.description}</div>
-      <button>Add to Cart</button>
-      </div>
-      `;
-      carListDOM.append(carItem);
+
+    try {
+      const response = await axios.get(
+        "https://car-shop-3164a-default-rtdb.firebaseio.com/cars.json"
+      );
+      const results = response.data;
+      for (let carId in results) {
+        console.log(
+          new Car(
+            results[carId].title,
+            results[carId].imgUrl,
+            results[carId].price,
+            results[carId].description
+          )
+        );
+        const carItem = new CarElement(
+          new Car(
+            results[carId].title,
+            results[carId].imgUrl,
+            results[carId].price,
+            results[carId].description
+          )
+        );
+        carListDOM.append(carItem.render());
+      }
+    } catch (error) {
+      console.log(error);
     }
 
     rootApp.append(header, carListDOM);
-  },
-};
+  }
+}
 
-carListOb.render();
+
+const app = new CarList();
+app.render();
